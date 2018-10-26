8eb67ea99b7edc6f1ab5fc1b1a6f5cd228e8008f
Ryker Rumsey <rykerrumsey@outlook.com>
2018-10-26 15:13:19 -0600

    remove un-nessasary projects

--- sql_exercise/main.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def main():
-    try:
-        conn = psycopg2.connect("dbname=postgres host=localhost user=postgres")
-    except:
-        print("I am unable to connect to the database")
-
-main()
